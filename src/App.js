eadb6a1c5cae2b3c69044c9005dbd1806bae4460
Abhinav Nagar <abhinav37nagar@gmail.com>
2021-05-27 19:44:13 +0530

    moved array from graph to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
+
 import Graph from "./components/Graph";
 
 function App() {
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    resetArray();
+  }, []);
+
+  const resetArray = () => {
+    var tempArray = Array(100).fill(null);
+    tempArray = tempArray.map((val, idx) => idx + 1);
+    for (var i = tempArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tempVal = tempArray[i];
+      tempArray[i] = tempArray[j];
+      tempArray[j] = tempVal;
+    }
+    setArray(tempArray);
+  };
+
   return (
     <div className="App">
-      <Graph />
+      <Graph array={array} />
     </div>
   );
 }
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,26 +1,6 @@
-import { useState, useEffect } from "react";
-
 import Bar from "./Bar";
 
-const Graph = () => {
-  const [array, setArray] = useState([]);
-
-  useEffect(() => {
-    resetArray();
-  }, []);
-
-  const resetArray = () => {
-    var tempArray = Array(100).fill(null);
-    tempArray = tempArray.map((val, idx) => idx + 1);
-    for (var i = tempArray.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var tempVal = tempArray[i];
-      tempArray[i] = tempArray[j];
-      tempArray[j] = tempVal;
-    }
-    setArray(tempArray);
-  };
-
+const Graph = ({ array }) => {
   return (
     <div className="graph">
       {array.map((val, idx) => (
