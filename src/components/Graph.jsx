a1e0f3e43ac6aeee67461c9daa8049efa1d58606
Abhinav Nagar <abhinav37nagar@gmail.com>
2021-05-27 19:26:24 +0530

    added reset function

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+
 const Graph = () => {
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    resetArray();
+  }, []);
+
+  const resetArray = () => {
+    var tempArray = Array(100).fill(null);
+    tempArray = tempArray.map((val, idx) => idx + 1);
+    for (var i = tempArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tempVal = tempArray[i];
+      tempArray[i] = tempArray[j];
+      tempArray[j] = tempVal;
+    }
+    setArray(tempArray);
+  };
+
   return <div className="graph"></div>;
 };
 
