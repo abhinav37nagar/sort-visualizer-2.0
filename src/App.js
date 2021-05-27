78fae7581bf91e7a6d776ccfef3a08084cbedc2f
Abhinav Nagar <abhinav37nagar@gmail.com>
2021-05-27 20:12:46 +0530

    added size state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { useState, useEffect } from "react";
 import Graph from "./components/Graph";
 import Header from "./components/Header";
 
+import { mergeSort } from "./algorithms/MergeSort";
+
 function App() {
+  const [size, setSize] = useState(10);
   const [array, setArray] = useState([]);
 
   useEffect(() => {
@@ -11,7 +14,7 @@ function App() {
   }, []);
 
   const resetArray = () => {
-    var tempArray = Array(100).fill(null);
+    var tempArray = Array(size).fill(null);
     tempArray = tempArray.map((val, idx) => idx + 1);
     for (var i = tempArray.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -22,9 +25,13 @@ function App() {
     setArray(tempArray);
   };
 
+  const runSort = () => {
+    mergeSort(array);
+  };
+
   return (
     <div className="App">
-      <Header resetArray={resetArray} />
+      <Header resetArray={resetArray} runSort={runSort} />
       <Graph array={array} />
     </div>
   );
